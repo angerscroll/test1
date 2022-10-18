fb2dab6ee26b786763becb5e0a9cfc72fecc8f36
angerscroll <angerscroll@gmail.com>
2022-10-18 17:41:22 +0900

    add getIndex

--- test1/test.js
+++ test1/test.js
@@ -16,11 +16,7 @@ const create_table = () => {
     const newRow = table.insertRow();
       for(y=0;y<l;y++){
         const newCell = newRow.insertCell(y);
-        newCell.addEventListener('click',() => {
-          const rowIndex = get_row_index(newRow);
-          const columnIndex = get_column_index(newCell);
-          get_index(rowIndex,columnIndex,newCell);
-        })
+        newCell.addEventListener('click',(e) => excute_event(e.target,newRow));
       }
   }
 };
@@ -41,13 +37,19 @@ const get_column_index = (cell) => {
   return cell.cellIndex;
 }
 
-const get_index = (r,c,n) => {
-  const cell =n;
+const excute_event = (cell,row) =>{
   change_color(cell);
+  get_index(cell,row)
+}
+const get_index = (cell,row) => {
+  const row_index = get_row_index(row);
+  const cell_index = get_column_index(cell);
+  console.log(`row : ${row_index}`);
+  console.log(`cell : ${cell_index}`);
 }
 
 const change_color = (cell) => {
-  var backgroundColor = cell.style.background;
+  var backgroundColor = cell.style.backgroundColor;
   if(backgroundColor=="black") cell.style.background = "white";
   else cell.style.background= "black";
 }
