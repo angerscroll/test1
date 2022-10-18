8770ef742b785ed4e7f5d87191a8a8572126031b
angerscroll <angerscroll@gmail.com>
2022-10-18 19:53:44 +0900

    make matrix and save value

--- test1/test.js
+++ test1/test.js
@@ -1,32 +1,53 @@
-const create = document.getElementById("create_button");
+var matrix;
 
-create.addEventListener('click',() =>{
-  create_table();
-});
+const create = document.getElementById("create_button");
+const save = document.getElementById("save_button");
+
+create.addEventListener('click',() =>create_table());
+save.addEventListener('click',() => save_value());
 
 
 const create_table = () => {
 
   remove_child("table");
-  const r = document.getElementById("row").value;
-  const l = document.getElementById("column").value;
+  const r = parseInt(document.getElementById("row").value);
+  const l = parseInt(document.getElementById("column").value);
+  
+  matrix = make_matrix(r,l);
+  console.log(matrix);
 
   const table = document.querySelector("table");
-  for(x=0;x<r;x++){
+  for(var x=0;x<r;x++){
     const newRow = table.insertRow();
-      for(y=0;y<l;y++){
+      for(var y=0;y<l;y++){
         const newCell = newRow.insertCell(y);
         newCell.addEventListener('click',(e) => excute_event(e.target,newRow));
       }
   }
 };
 
-const remove_child = (id)=>{
-  
-  const remove_node = document.getElementById(id);
+const change_data = (idx) => {
+  const row = idx[0];
+  const cell =idx[1];
+  if(matrix[row][cell] == 0 ) matrix[row][cell] = 1;
+  else if (matrix[row][cell] == 1) matrix[row][cell] = 0;
+  else console.log(err);
+  console.log(matrix);
+}
+const make_matrix = (row,cell) => {
+  var matrix = new Array(row);
+  matrix.fill(0);
+  for(var i=0;i<matrix.length;i++) {
+    inner = new Array(cell);
+    inner.fill(0);
+    matrix[i] = inner;
+  }
+  return matrix;
+}
 
+const remove_child = (id)=>{
+  const remove_node = document.getElementById(id);
   remove_node.textContent = "";
-  
 };
 
 const get_row_index = (row) => {
@@ -39,17 +60,29 @@ const get_column_index = (cell) => {
 
 const excute_event = (cell,row) =>{
   change_color(cell);
-  get_index(cell,row)
+  change_data(get_index(cell,row));
 }
 const get_index = (cell,row) => {
   const row_index = get_row_index(row);
   const cell_index = get_column_index(cell);
-  console.log(`row : ${row_index}`);
-  console.log(`cell : ${cell_index}`);
+  return [row_index,cell_index];
 }
 
 const change_color = (cell) => {
   var backgroundColor = cell.style.backgroundColor;
   if(backgroundColor=="black") cell.style.background = "white";
   else cell.style.background= "black";
+}
+
+const save_value = () => {
+  var arr = matrix;
+  const row = arr.length;
+  const cell = arr[0].length;
+  var newArr = new Array();
+  for(var intIndex = 0;intIndex<arr.length;intIndex++){
+    var innerValue = arr[intIndex].join('');
+    newArr.push(innerValue);
+  }
+  var value = newArr.join('');
+  console.log(value);
 }
